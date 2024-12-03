577e4b96b39f861b6a5a9b156dcf3bfb34158193
aliabbascheema <s290082@uos.ac.uk>
2024-12-03 20:38:03 +0300

    Added models to admin pages for management

--- scheema_retail_store/admin.py
+++ scheema_retail_store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cart, CartItem, Product, Category, Order, OrderItem, Review
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Cart)
+admin.site.register(CartItem)
